7c78319e530bae5f04ccf13982bdd5675df12375
bdhee <bdheerajbanu@gmail.com>
2026-04-24 23:02:06 +0530

    Initial commit - Spring Boot Employee API

---
+++ src/main/java/com/api_nov/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.api_nov.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity <String> runtimeExceptionHandler(RuntimeException e,WebRequest request) {
+        return new ResponseEntity<>( e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
